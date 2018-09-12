281791bfe679ccdd9905345cd82ecf0932fef233
carlosJimenez2 <42255224+carlosJimenez2@users.noreply.github.com>
2018-09-11 20:54:21 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArreglosMultidimensionales
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int j;
+            int[,] matriz1 = new int [ 5, 5 ];
+            int[,] matriz3 = { { 3, 4 }, { 5, 6 } };
+            int [,] matriz4 = { { 7, 8 }, { 9, 10 } };
+            string[,] matriz5 = { { "Enero", "Lunes" }, { "Soleado", "Lluvioso" } };
+            string[,] matriz6 = { { "mes", "dia" }, { "Clima en la añana", "Clima en la tarde" } };
+
+            for (i = 0; i < 2; i++)
+            {
+                for (j = 0; i < 2; j++)
+                {
+                    matriz1[i,j] = matriz3[i,j] + matriz4[i,j];
+                    Console.WriteLine(matriz1[i,j]);
+                }
+            } 
+
+          /*  for (i = 0; i < 2; i++)
+            {
+                for (j = 0; i < 2; j++)
+                {
+                   
+                    Console.WriteLine("El {0}, Sera {1}", matriz6[i, j], matriz5[i,j]);
+                }
+            }*/
+
+           
+            Console.ReadKey();
+
+        }
+    }
+}
